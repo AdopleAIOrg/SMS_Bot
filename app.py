546c796355eb9c4e85f6db70b108e1115a810a62
srinivasanbadople <82161848+srinivasanbadople@users.noreply.github.com>
2023-12-23 15:29:25 +0530

    Adding Twilo Receive SMS Method
    Signed-off-by: srinivasanbadople <82161848+srinivasanbadople@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import flask
-from flask import request, jsonify
+from flask import Flask, request, jsonify
+from twilio import twiml
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -13,6 +14,16 @@ def recive_message():
     input_msg = request.get_json()    
     print("input_msg", input_msg)
     return jsonify({'success': True, 'message': input_msg['content'] })
+
+
+@app.route('/sms', methods=['POST'])
+def sms():
+    number = request.form['From']
+    message_body = request.form['Body']
+
+    resp = twiml.Response()
+    resp.message('Hello {}, you said: {}'.format(number, message_body))
+    return str(resp)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
